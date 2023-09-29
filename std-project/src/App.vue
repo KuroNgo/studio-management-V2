ffe6fa4611df90c7863d19e93b5b61a7b0240e18
Hoang21002 <dinhhoang026@gmail.com>
2023-09-30 02:26:13 +0700

    feat: Add Banner

--- std-project/src/App.vue
+++ std-project/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <div v-if="show" class="banner"></div>
   <header-view v-if="showHeaderFooter" />
   <router-view></router-view>
   <!-- <router-view v-slot="{ Component }">
@@ -20,9 +21,17 @@ export default {
     showHeaderFooter() {
       // Truy cập giá trị showHeaderFooter từ meta data của tuyến hiện tại
       return this.$route.meta.showHeaderFooter;
+    },
+    show() {
+      return this.$route.name == 'Trang chủ | Cỏ Studio'
     }
   }
-
 }
 </script>
-<style></style>
+<style>
+.banner {
+  width: 100%;
+  height: 200px;
+  background: url(../public/assets/images/banner.jpg) no-repeat center/cover;
+}
+</style>
